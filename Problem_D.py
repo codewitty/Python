ce16736b65b07080be118ae0fdef02e2537a07ea
Joshua Gomes <gomesjoshua@gmail.com>
2020-05-01 15:38:12 -0400

    Unit D take home finished

--- Problem_D.py
+++ Problem_D.py
@@ -3,6 +3,7 @@
 # Unit D take-home assignment
 
 # Part One - Sets
+from collections import namedtuple
 
 class1 = {'Li', 'Audry', 'Jia', 'Migel', 'Tanya'}
 class2 = {'Sasha', 'Migel', 'Tanya', 'Hiroto', 'Audry'}
@@ -10,7 +11,7 @@ class3 = {'Migel', 'Zhang', 'Hiroto', 'Anita', 'Jia'}
 
 print (f'Students in all three classes: {sorted(class1 & class2 & class3)}')
 print (f'All students: {sorted(class1 | class2 | class3)}')
-print (f'All students: {sorted((class1 - class2) & (class1 - class3))}')
+print (f'Students in class1 but not class2 or class3: {sorted((class1 - class2) & (class1 - class3))}')
 
 # Part Two - Swap
 
@@ -25,19 +26,27 @@ title, year, genre = movie
 
 print (f'My favorite movie is: {title}')
 
+# Part Four - Named Tuple
+Movie = namedtuple('Movie', 'title year genre')
+favMovie = Movie(title = "Casablanca", year = 1942, genre = "romantic drama")
+print (f'My favorite movie is: {favMovie.title}')
+
+# Part Five - Named Tuple Containing a List
+Moviestars = namedtuple('Moviestars', 'title year genre stars')
+favoritemovie = Moviestars(title = "Casablanca", year = 1942, genre = "romantic drama", stars = ['Humphrey Bogart', 'Ingrid Bergman'])
+favoritemovie.stars.append('Claude Rains')
+print (f'My favorite star is: {favoritemovie.stars[1]}')
+print (f'{favoritemovie}')
 
 '''
 Execution Results:
 
-apple dessert is: sauce
-banana dessert exists: True
-pear dessert exists: False
-dict_keys(['apple', 'peach', 'strawberry', 'banana', 'mango'])
-dict_values(['sauce', 'cobbler', 'shortcake', 'cream pie', 'sticky rice'])
-dict_items([('apple', 'sauce'), ('peach', 'cobbler'), ('strawberry', 'shortcake'), ('banana', 'cream pie'), ('mango', 'sticky rice')])
-Sorted state capitols: ['Denver', 'Hartford', 'Juneau', 'Little Rock', 'Montgomery', 'Phoenix', 'Sac.']
-Students in both classes: {'Tanya', 'Migel', 'Audry'}
-All students: {'Tanya', 'John', 'Sasha', 'Hiroto', 'Jia', 'Audry', 'Li', 'Migel'}
-Sasha is in class1: False
-
+Students in all three classes: ['Migel']
+All students: ['Anita', 'Audry', 'Hiroto', 'Jia', 'Li', 'Migel', 'Sasha', 'Tanya', 'Zhang']
+Students in class1 but not class2 or class3: ['Li']
+List after swap: [1, 3, 2]
+My favorite movie is: Casablanca
+My favorite movie is: Casablanca
+My favorite star is: Ingrid Bergman
+Moviestars(title='Casablanca', year=1942, genre='romantic drama', stars=['Humphrey Bogart', 'Ingrid Bergman', 'Claude Rains'])
 '''
