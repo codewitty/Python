1e7b07dd81fbefc29c925228d37db77226a1095e
Joshua Gomes <gomesjoshua@gmail.com>
2020-04-30 20:45:00 -0400

    Unit D completed

--- UnitD_InClass.py
+++ UnitD_InClass.py
@@ -25,16 +25,46 @@ print (f'{sweets.values()}')
 print (f'{sweets.items()}')
 
 # Combining Dictionaries
+capitols1 = {
+	 'Alabama': 'Montgomery',
+	 'Alaska': 'Juneau',
+	 'Arizona': 'Phoenix',
+	 'Arkansas': 'Little Rock',
+	 }
 
+capitols2 = {
+	 'California': 'Sac.',
+	 'Colorado': 'Denver',
+	 'Connecticut': 'Hartford',
+	 }
 
+capitols1.update(capitols2)
+print (f'Sorted state capitols: {sorted(capitols1.values())}')
 
+# Set Basics
 
+class1 = {'Li', 'Audry', 'Jia', 'Migel', 'Tanya'}
+class2 = {'Sasha', 'Migel', 'Tanya', 'Hiroto', 'Audry'}
 
+class1.add('John')
 
-
+print (f'Students in both classes: {class1 & class2}')
+print (f'All students: {class1 | class2}')
+print (f'Sasha is in class1: {"Sasha" in class1}')
 
 
 '''
 Execution Results:
+
+apple dessert is: sauce
+banana dessert exists: True
+pear dessert exists: False
+dict_keys(['apple', 'peach', 'strawberry', 'banana', 'mango'])
+dict_values(['sauce', 'cobbler', 'shortcake', 'cream pie', 'sticky rice'])
+dict_items([('apple', 'sauce'), ('peach', 'cobbler'), ('strawberry', 'shortcake'), ('banana', 'cream pie'), ('mango', 'sticky rice')])
+Sorted state capitols: ['Denver', 'Hartford', 'Juneau', 'Little Rock', 'Montgomery', 'Phoenix', 'Sac.']
+Students in both classes: {'Tanya', 'Migel', 'Audry'}
+All students: {'Tanya', 'John', 'Sasha', 'Hiroto', 'Jia', 'Audry', 'Li', 'Migel'}
+Sasha is in class1: False
 
 '''
