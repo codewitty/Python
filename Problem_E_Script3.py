3170472faefb90e9d7bfae05495af25d5d0339da
Joshua Gomes <gomesjoshua@gmail.com>
2020-05-13 00:54:14 -0400

    unitf

--- Problem_E_Script3.py
+++ Problem_E_Script3.py
@@ -22,14 +22,6 @@ for i in range(1, num + 1):
 	print ("\n")
 
 
-
-
-
-
-
-
-
-
 '''
 Execution results:
 
---
+++ UnitF_InClass.py
@@ -0,0 +1,36 @@
+# Joshua Nelson Gomes (Joshua)
+# CIS 41A Spring 2020
+# In-Class Assignment F
+
+
+def hello
+	'hello prints the message "Hello World"'
+	print (f'Hello World')
+	
+
+
+
+
+
+
+'''
+Execution results:
+Check ==
+list1 == list2:  True
+list1 == list3:  True
+list2 == list3:  True
+
+Check "is"
+list1 is list2:  True
+list1 is list3:  False
+list2 is list3:  False
+
+Print i.d
+List1 ID:  4371886960
+List2 ID:  4371886960
+List3 ID:  4371966144
+
+List1 printed:  [2, 'goodbye', 4.1, 'hello', 8]
+List2 printed:  [2, 'goodbye', 4.1, 'hello', 8]
+List3 printed:  [4.1, 'hello']
+'''
