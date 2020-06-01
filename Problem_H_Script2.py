6c6cab2445732aefb886ef236ed05aad3503fc4d
Joshua Gomes <gomesjoshua@gmail.com>
2020-06-01 01:19:44 -0400

    Problem H

--- Problem_H_Script2.py
+++ Problem_H_Script2.py
@@ -17,28 +17,41 @@ class BritCoins:
 			self.totalPennies += (kwargs.get('penny') * BritCoins.__coinValues.get('penny'))
 
 	def __add__(self, other):
+		total = self.totalPennies + other.totalPennies
+		return BritCoins(penny = total)
 		
+	def __sub__(self, other):
+		total = self.totalPennies - other.totalPennies
+		return BritCoins(penny = total)
 
 	def __str__(self):
-		return '{self.totalPennies}'.format(self=self)
-	def displayState(self):
-		print(f'{self.name} ({self.abbreviation}) is in the {self.region} region and has population of {self.population}')
+		self.p = self.totalPennies // BritCoins.__coinValues.get('pound')
+		self.s = (self.totalPennies % BritCoins.__coinValues.get('pound')) // BritCoins.__coinValues.get('shilling')
+		self.pe = self.totalPennies - (self.p * BritCoins.__coinValues.get('pound') + self.s *  BritCoins.__coinValues.get('shilling'))
+		return '{self.p} pounds {self.s} shillings {self.pe} pennies'.format(self=self)
+
 
 def main():
-	c1 = BritCoins(pound = 4, shilling = 3, penny = 2)
+	c1 = BritCoins(pound = 4, shilling = 24, penny = 3)
+	c2 = BritCoins(pound = 3, shilling = 4, penny = 5)
+	c3 = c1 + c2
+	c4 = c1 - c2
 
 	print(c1)
+	print(c2)
+	print(c3)
+	print(c4)
 
 
 if __name__ == '__main__':
     main()
+
 '''
 Execution Results:
 
-Warning: temperature is 550
-Misc error #404
-Warning: CO2 level is 0.18
-Warning: CO2 level is 0.2
-Misc error #418
+5 pounds 4 shillings 3 pennies
+3 pounds 4 shillings 5 pennies
+8 pounds 8 shillings 8 pennies
+1 pounds 19 shillings 10 pennies
 
 '''
