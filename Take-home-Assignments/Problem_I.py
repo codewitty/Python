47c9892d65cbf5465da9b57ef0f2016bb1aaba17
Joshua Gomes <gomesjoshua@gmail.com>
2020-06-06 23:06:42 -0400

    Problem I near complete :

--- Take-home-Assignments/Problem_I.py
+++ Take-home-Assignments/Problem_I.py
@@ -14,36 +14,71 @@ class LibraryPatron:
 			self.booksCheckedOut.append(bookTitle)
 			print (f'{self.name} has checked out {bookTitle}')
 		else:
-			print (f'Sorry {self.name} you are at your limit of {checkOutLimit} books')
+			print (f'Sorry {self.name} you are at your limit of {self.checkOutLimit} books')
 
 	def returnBook(self, book):
 		bookName = book[0]
-		self.booksCheckedOut.pop[bookName]
-		
+		self.booksCheckedOut.remove(bookName)
 		print (f'{self.name} has returned {bookName}')
+		self.checkOutLimit += 1
+
+	def printCheckedOutBooks(self):
+			print (f'{self.name} has the following books checked out:')
+			print (f' {self.booksCheckedOut}')
 
 class AdultPatron(LibraryPatron):
 	def __init__(self, name):
-		Circle.__init__(self, name)
+		LibraryPatron.__init__(self, name)
 		self.checkOutLimit = 4
 
 	def checkOutBook(self, book):
-		if (checkOutLimit >= 1):
-			self.booksCheckedOut.append(bookTitle)
-			print (f'{self.name} has checked out {bookTitle}')
+		if (self.checkOutLimit >= 1):
+			LibraryPatron.checkOutBook(self, self.checkOutLimit, book[0])
+			self.checkOutLimit -= 1
 		else:
-			print (f'Sorry {self.name} you are at your limit of {checkOutLimit} books')
-	def getVolume(self):
-		return Circle.getArea(self) * self.height
+			print (f'Sorry {self.name} you are at your limit of 4 books')
+
+class JuvenilePatron(LibraryPatron):
+	def __init__(self, name):
+		LibraryPatron.__init__(self, name)
+		self.checkOutLimit = 2
+
+	def checkOutBook(self, book):
+		if (book[1] == "Adult"):
+			print (f'Sorry {self.name} this is book is only for adult patrons.') 
+
+		elif (self.checkOutLimit >= 1 and book[1] == "Juvenile"):
+			LibraryPatron.checkOutBook(self, self.checkOutLimit, book[0])
+			self.checkOutLimit -= 1
+
+		else:
+			print (f'Sorry {self.name} you are at your limit of 2 books')
 
 def main():
 
-	circle = Circle(4)
-	print (f'Circle area is: {circle.getArea():.2f}')
-	print(circle.radius)
-	print(circle.getArea())
-	cylinder = Cylinder(2, 5)
-	print (f'Cylinder volume is: {cylinder.getVolume():.2f}')
+	book1 = ["Alice in Wonderland", "Juvenile"]
+	book2 = ["The Cat in the Hat", "Juvenile"]
+	book3 = ["Harry Potter and the Sorcerer's Stone", "Juvenile"]
+	book4 = ["The Hobbit", "Juvenile"]
+	book5 = ["The Da Vinci Code", "Adult"]
+	book6 = ["The Girl with the Dragon Tattoo", "Adult"]
+
+
+	patron1 = JuvenilePatron("Jimmy")
+	patron2 = AdultPatron("Sophia")
+
+	patron1.checkOutBook(book6)
+	patron1.checkOutBook(book1)
+	patron1.checkOutBook(book2)
+	patron1.printCheckedOutBooks()
+	patron1.checkOutBook(book3)
+	patron1.returnBook(book1)
+	patron1.checkOutBook(book3)
+	patron1.printCheckedOutBooks()
+	patron2.checkOutBook(book5)
+	patron2.checkOutBook(book4)
+	patron2.printCheckedOutBooks()
+
 
 if __name__ == '__main__':
     main()
