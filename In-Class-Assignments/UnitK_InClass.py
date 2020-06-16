7c475f3bf00171af71c8df6341a450ee42fd2e33
Joshua Gomes <gomesjoshua@gmail.com>
2020-06-15 20:58:50 -0400

    Unit K complete

---
+++ In-Class-Assignments/UnitK_InClass.py
@@ -0,0 +1,15 @@
+# Joshua Nelson Gomes (Joshua)
+# CIS 41A Spring 2020
+# In-Class Assignment Unit K
+
+import random
+
+print(f'{sorted(random.sample([*range(1, 16)], 10))}')
+
+
+'''
+Execution Results:
+
+[1, 2, 4, 6, 7, 9, 11, 13, 14, 15]
+
+'''
