535499fbff56d82e7b8a25761b839a29a65af220
Joshua Gomes <gomesjoshua@gmail.com>
2020-06-20 23:38:41 -0400

    update problem j

--- Take-home-Assignments/Problem_J.py
+++ Take-home-Assignments/Problem_J.py
@@ -7,19 +7,19 @@ import random
 def diceGame():
 	prize = 0
 	a = True
-	dice_dict = {4:4, 5:5, 6:6}
 	while a == True:
 		dice = random.randint(1, 6)
 		if (dice < 4):
 			a = False
 		else:
-			prize += dice_dict.get(dice) 
+			prize += dice
 	return prize
 
 def main():
 	max_prize = 0
+	game = 10000
 	total_prize = 0
-	for i in range(0, 10000):
+	for i in range(0, game):
 		game_prize = diceGame()
 		if game_prize > max_prize:
 			max_prize = game_prize
